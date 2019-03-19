860b9472f8ac299fbedde289abd5e7c545604818
Odair Cardoso Martins <omartins@fitesa.com.br>
2019-03-18 22:11:56 -0300

    teste

---
+++ precache-manifest.2d4a1be97f4b9c8b1267688764efda9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pecuaria/robots.txt"
+  },
+  {
+    "revision": "8005feddc5e6c33fd9a4",
+    "url": "/pecuaria/js/chunk-vendors.14bc9a0f.js"
+  },
+  {
+    "revision": "fb1f6794273211af5acc",
+    "url": "/pecuaria/js/app.d2b5e810.js"
+  },
+  {
+    "revision": "45cc729f595c294a2893",
+    "url": "/pecuaria/js/about.687cbed7.js"
+  },
+  {
+    "revision": "38f16248ec6fb4dc1157652f3a96a018",
+    "url": "/pecuaria/index.html"
+  },
+  {
+    "revision": "fefaa81dbf17064d92796d32f5f116d6",
+    "url": "/pecuaria/img/logo-cow.fefaa81d.png"
+  },
+  {
+    "revision": "8005feddc5e6c33fd9a4",
+    "url": "/pecuaria/css/chunk-vendors.fa25aca3.css"
+  }
+];
---
+++ registerServiceWorker.js
@@ -0,0 +1,35 @@
+importScripts("/pecuaria/precache-manifest.2d4a1be97f4b9c8b1267688764efda9e.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/* eslint-disable no-console */
+
+import { register } from 'register-service-worker'
+
+if (process.env.NODE_ENV === 'production') {
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    ready () {
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      )
+    },
+    registered () {
+      console.log('Service worker has been registered.')
+    },
+    cached () {
+      console.log('Content has been cached for offline use.')
+    },
+    updatefound () {
+      console.log('New content is downloading.')
+    },
+    updated () {
+      console.log('New content is available; please refresh.')
+    },
+    offline () {
+      console.log('No internet connection found. App is running in offline mode.')
+    },
+    error (error) {
+      console.error('Error during service worker registration:', error)
+    }
+  })
+}
+
